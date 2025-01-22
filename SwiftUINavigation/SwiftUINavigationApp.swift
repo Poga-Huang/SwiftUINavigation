4e313569bd1fef928b85077b33b6626537929e0b
poga07 <chris@168money.com.tw>
2025-01-22 15:31:17 +0800

    Initial Commit

---
+++ SwiftUINavigation/SwiftUINavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationApp.swift
+//  SwiftUINavigation
+//
+//  Created by 黃柏嘉 on 2025/01/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
